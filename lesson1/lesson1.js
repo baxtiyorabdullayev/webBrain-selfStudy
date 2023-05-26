f10f7eb0cac03b9a6047a3b37a541c480f63e133
Bakhtiyor Abdullaev <bakhtiyor.abdullaev.frontend@gmail.com>
2023-05-26 15:16:05 +0500

    1st lesson is over

--- lesson1/lesson1.js
+++ lesson1/lesson1.js
@@ -1,1 +1,29 @@
-console.log("lesson 1");
+const fruits = ["olma", "anor", "banan", "nok", "shaftoli"];
+const lengths = [];
+
+fruits.forEach((element) => {
+  lengths.push(element.length);
+});
+console.log(Math.max(...lengths));
+
+// data types
+
+// primitive data types
+let num = 3;
+let str = "hey";
+let booln = true;
+let nul = null;
+let undf = undefined;
+let bigint = 5n;
+let symbl = Symbol("unique id");
+
+// non-primitive data types
+const obj = {
+  name: "Me",
+  age: 5,
+};
+
+const arr = [1, 2, 3];
+const myFn = () => {
+  console.log("heyyy");
+};
