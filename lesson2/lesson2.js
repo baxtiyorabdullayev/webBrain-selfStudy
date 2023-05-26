c2da7430497b978cb94d87d6c90c6f0725d86c24
Bakhtiyor Abdullaev <bakhtiyor.abdullaev.frontend@gmail.com>
2023-05-27 00:37:25 +0500

    2nd lesson

---
+++ lesson2/lesson2.js
@@ -0,0 +1,16 @@
+let a = "22.4";
+console.log(Number.parseFloat(a));
+console.log(Number.parseInt(a));
+
+let num = 4.5;
+console.log(parseFloat(num));
+console.log(parseInt(num));
+
+let text1 = "a";
+let text2 = "A";
+console.log(text1 > text2);
+
+let text3 = "js";
+let text4 = "Somebody";
+
+console.log(text3 < text4);
