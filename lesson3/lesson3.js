61840964d174e83813b05f6a2ed3d07f2bf63683
Bakhtiyor Abdullaev <bakhtiyor.abdullaev.frontend@gmail.com>
2023-05-27 20:54:38 +0500

    if else, ternery operator, nullish operator

---
+++ lesson3/lesson3.js
@@ -0,0 +1,9 @@
+// alert("dfg");
+// Nullish opeator
+// let a = undefined;
+// let b = 1;
+
+// console.log(a ?? b);
+// console.log(a || b);
+
+console.log(undefined ?? null ?? "he" ?? true ?? null ?? false);
